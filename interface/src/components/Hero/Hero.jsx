2ada9db3d9493685f9fd076cc70e3940e0e1b0db
MarcioDzn <79764696+MarcioDzn@users.noreply.github.com>
2024-07-01 13:54:42 -0300

    :lipstick: feat: change landing page design

--- interface/src/components/Hero/Hero.jsx
+++ interface/src/components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
-import { HeroContainer, HeroInfo } from "./HeroStyled";
+import { Button, HeroContainer, HeroInfo } from "./HeroStyled";
+import { Link } from "react-router-dom"
 
 export function Hero() {
     return (
         <HeroContainer>
             <HeroInfo>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                <h1><span>Seguro</span> e <span>Descentralizado</span>. <br/>Feito para isso.</h1>
+                <p>Experimente um novo jeito de cuidar do seu dinheiro. Abra sua conta hoje e descubra como é simples e seguro transformar seus sonhos em realidade com o Unified Bank.</p> 
+                <Link to="/register">
+                    <Button type="submit" btn_type="bold">Abra a sua conta</Button>
+                </Link>
             </HeroInfo>
         </HeroContainer>
     )
--- interface/src/components/Hero/HeroStyled.jsx
+++ interface/src/components/Hero/HeroStyled.jsx
@@ -1,15 +1,47 @@
 import {styled} from "styled-components"
 
 export const HeroContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
+    height: 100%;
     padding: 100px 250px;
     border-bottom: 1px solid gray;
 `
 
 export const HeroInfo = styled.span`
     display: block;
-    width: 500px;
     height: 300px;
-    font-size: 3rem;
-    font-weight: bold;
+    width: 100%;
+    text-align: center;
+
+    h1 {
+        font-weight: 700;
+        font-size: 4rem;
+        line-height: 4rem;
+
+    }
+
+    p {
+        width: 510px;
+        font-size: 1.2rem;
+        margin: auto;
+        margin-top: 25px;
+    }
+`
+
+export const Button = styled.button`
+    border: ${(props) => props.btn_type === "bold" ? "1px solid black" : "1px solid white"};
+    border-radius: 10px;
+    color: ${(props) => props.btn_type === "bold" ? "#ffffff" : "black"};
+    background-color: ${(props) => props.btn_type == "bold" ? "#c40000" : "white"};
+    padding: 10px 20px;
+    cursor: pointer;
+    margin-top: 40px;
+
+    transition: ${(props) => props.btn_type == "bold" ? "border-radius .15s" : "color: 0.5s"};
+    &:hover {
+        ${(props) => props.btn_type == "bold" ? "border-radius: 60px" : "color: #c40000"} ;
+    }
 `
