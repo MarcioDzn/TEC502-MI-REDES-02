5fb614d4dbf6ed285ee7c0aab74502061b09e6e5
MarcioDzn <79764696+MarcioDzn@users.noreply.github.com>
2024-07-02 20:51:52 -0300

    :lipstick: feat: add landing page animations

--- interface/src/components/Hero/Hero.jsx
+++ interface/src/components/Hero/Hero.jsx
@@ -5,7 +5,8 @@ export function Hero() {
     return (
         <HeroContainer>
             <HeroInfo>
-                <h1><span>Seguro</span> e <span>Descentralizado</span>. <br/>Feito para isso.</h1>
+                <h1 className="reveal-text"><span>Seguro</span> e <span>Descentralizado</span>. <br/><span className="highlight-word">Feito para isso.<div className="highlight"></div></span></h1>
+                
                 <p>Experimente um novo jeito de cuidar do seu dinheiro. Abra sua conta hoje e descubra como é simples e seguro transformar seus sonhos em realidade com o Unified Bank.</p> 
                 <Link to="/register">
                     <Button type="submit" btn_type="bold">Abra a sua conta</Button>
--- interface/src/components/Hero/HeroStyled.jsx
+++ interface/src/components/Hero/HeroStyled.jsx
@@ -20,7 +20,6 @@ export const HeroInfo = styled.span`
         font-weight: 700;
         font-size: 4rem;
         line-height: 4rem;
-
     }
 
     p {
@@ -29,6 +28,63 @@ export const HeroInfo = styled.span`
         margin: auto;
         margin-top: 25px;
     }
+
+    .reveal-text {
+        opacity: 0;
+        animation: reveal 1s forwards; /* Aplica a animação 'reveal' por 1 segundo */
+    }
+
+    @keyframes reveal {
+        from {
+            opacity: 0;
+            transform: translateY(20px); /* Começa com o texto um pouco abaixo */
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0); /* Termina com o texto na posição normal */
+        }
+    }
+
+    .highlight {
+    position: absolute; /* Posição absoluta para cobrir a palavra */
+    top: 50%; /* Alinha verticalmente no meio do contêiner */
+    transform: translateY(-50%); /* Ajusta para alinhar verticalmente */
+    left: 0;
+    height: 100%; /* Cobrir altura total */
+    background-color: #c40000; /* Cor do marca texto */
+    width: 0; /* Começa com largura zero para esconder o highlight */
+    z-index: -1; /* Coloca o highlight abaixo do texto */
+    animation: reveal-highlight 1s cubic-bezier(0.4, 0, 0.1, 1) 0.6s; /* Aplica a animação 'reveal-highlight' */
+    animation-fill-mode: forwards;
+    }
+
+    .highlight-word {
+    display: inline-block; /* Garante que a palavra seja tratada como um bloco */
+    position: relative; /* Posição relativa para que o highlight absoluto seja posicionado corretamente */
+    z-index: 2;
+    animation: change-color 1s cubic-bezier(0.4, 0, 0.1, 1) 1.5s; /* Aplica a animação 'reveal-highlight' */
+    animation-fill-mode: forwards;
+    }
+
+    @keyframes reveal-highlight {
+    from {
+        width: 0; /* Começa com largura zero para esconder o highlight */
+    }
+    to {
+        width: 100%; /* Termina com largura total para revelar o highlight completamente */
+    }
+    }
+
+    @keyframes change-color {
+    from {
+       color: black; /* Começa com largura zero para esconder o highlight */
+    }
+    to {
+        color: white; /* Termina com largura total para revelar o highlight completamente */
+    }
+    }
+
+
 `
 
 export const Button = styled.button`
@@ -45,3 +101,4 @@ export const Button = styled.button`
         ${(props) => props.btn_type == "bold" ? "border-radius: 60px" : "color: #c40000"} ;
     }
 `
+
